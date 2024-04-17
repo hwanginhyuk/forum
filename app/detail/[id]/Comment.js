749d4b6f6b49a97909cdb5e220b830de1ada385c
INHYUK\fit99 <fit9972@gmail.com>
2024-04-17 11:58:04 +0900

    ver 1.2 댓글기능 추가

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -1,25 +1,43 @@
 'use client'
-import { useState, useEffect  } from 'react';
+import { useState, useEffect } from 'react';
 
-export default  function Comment(props){
+export default function Comment(props) {
 
     let [comment, setComment] = useState('')
+    let [data, setData] = useState([])
 
-    useEffect(()=>{
-        fetch()
-    },[])
+    useEffect(() => {
+        // 컴포넌트 로드시 서버에서 댓글을 가져온다
+        fetch('/api/comment/list?id=' + props._id).then(r=>r.json())
+            .then((result) => {
+                // console.log(result)
+                setData(result)
+                // console.log(data)
+            })
+    }, [])
 
-    return(
+    return (
         <div>
             <div>댓글목록</div>
-            <input onChange={(e)=>{ setComment(e.target.value) }}/>
+            {
+                data.length > 0 ?
+                data.map((a, i) => {
+                    return (
+                        <div>
+                        <p key={i}>{a.content}</p>
+                        </div>
+                    )
+                })
+                : '댓글없음'
+            }
+            <input onChange={(e) => { setComment(e.target.value) }} />
             <button
-                onClick={ ()=>  {
+                onClick={() => {
                     fetch('/api/comment/new',
-                    { 
-                        method : 'POST', 
-                        body : JSON.stringify({ comment :comment, _id : props._id })
-                    })
+                        {
+                            method: 'POST',
+                            body: JSON.stringify({ comment: comment, _id: props._id })
+                        }).then()
                 }}
             >댓글전송</button>
         </div>
--- pages/api/comment/new.js
+++ pages/api/comment/new.js
@@ -6,9 +6,9 @@ import { ObjectId } from "mongodb";
 export default async function handler(요청, 응답){
 
     let session = await getServerSession(요청, 응답, authOptions)
-    if (session){
-        session.user.email = 요청.body.email 
-    }
+
+    // console.log(session)
+    
     if(요청.method == 'POST'){
         요청.body = JSON.parse(요청.body)
         let 저장할거 = {
@@ -19,6 +19,6 @@ export default async function handler(요청, 응답){
 
         const db = (await connectDB).db('forum')
         let result = await db.collection('comment').insertOne(저장할거)
-        응답.status(200).json('저장완료')
+        응답.status(200).json('댓글달기성공')
     }
 }
